ffb1746b674258daae5fe10c06a91bfc73e8393b
jpacheco2000 <jpacheco2000@gmail.com>
2021-07-22 15:28:29 -0400

    inicio
    inicio

---
+++ tarea2-01.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#define tam 2073600
+
+int main (int argc, char **argv){
+    FILE *arch;
+    arch = fopen(argv[1], "rb");
+    if (arch == NULL){
+        exit(1);
+    }
+    int vec[tam];
+    fread(vec, sizeof(int), tam, arch);
+    int f;
+    for (f = 0; f<tam; f++) {
+    printf("%i\n", vec[f]);
+    }
+    fclose(arch);
+    
+}
+
