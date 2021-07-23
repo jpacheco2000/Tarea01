9b246c18d09eb8d04f7da84c444d8ab5a888e761
Juan Pacheco <jpacheco2000@gmail.com>
2021-07-23 17:11:31 +0000

    todo

--- tarea2-01.c
+++ tarea2-01.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #define tam 2073600
 
 int main (int argc, char **argv){
